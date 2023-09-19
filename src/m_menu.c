23d7a13ae99f836d7d0713e42a2433c8e304b9b9
Alaux <73968015+MrAlaux@users.noreply.github.com>
2023-09-19 20:54:05 -0300

    Comments, readme and changelog update

--- src/m_menu.c
+++ src/m_menu.c
@@ -4143,8 +4143,8 @@ enum {
   gen2_sndresampler,
   gen2_sndmodule,
   gen2_sndhrtf,
-  gen2_sndabsorption,
-  gen2_snddoppler,
+  gen2_sndabsorption, // [Nugget]
+  gen2_snddoppler, // [Nugget]
   gen2_gap2,
 
   gen2_musicbackend,
@@ -4208,8 +4208,8 @@ static const char *sound_resampler_menu_strings[] = {
 static void M_UpdateAdvancedSoundItems(void)
 {
   DISABLE_ITEM(snd_module != SND_MODULE_3D, gen_settings2[gen2_sndhrtf]);
-  DISABLE_ITEM(snd_module != SND_MODULE_3D, gen_settings2[gen2_sndabsorption]);
-  DISABLE_ITEM(snd_module != SND_MODULE_3D, gen_settings2[gen2_snddoppler]);
+  DISABLE_ITEM(snd_module != SND_MODULE_3D, gen_settings2[gen2_sndabsorption]); // [Nugget]
+  DISABLE_ITEM(snd_module != SND_MODULE_3D, gen_settings2[gen2_snddoppler]); // [Nugget]
 }
 
 static void M_SetSoundModule(void)
@@ -4341,9 +4341,11 @@ setup_menu_t gen_settings2[] = { // General Settings screen2
   {"Headphones Mode", S_YESNO, m_null, M_X,
    M_Y + gen2_sndhrtf*M_SPC, {"snd_hrtf"}, 0, M_SetSoundModule},
 
+  // [Nugget]
   {"Air Absorption", S_THERMO, m_null, M_X_THRM,
    M_Y + gen2_sndabsorption*M_SPC, {"snd_absorption"}, 0, M_UpdateUserSoundSettings},
 
+   // [Nugget]
   {"Doppler Effect", S_THERMO, m_null, M_X_THRM,
    M_Y + gen2_snddoppler*M_SPC, {"snd_doppler"}, 0, M_UpdateUserSoundSettings},
 
